4434a9c673a67f5bb5c952123f7582c646c7c5f4
bumfo <bumfo@users.noreply.github.com>
2021-10-11 06:01:27 +0000

    add metal code

--- macos/com_scalarmachine_skijaawt_SkiaMetalCanvas.mm
+++ macos/com_scalarmachine_skijaawt_SkiaMetalCanvas.mm
@@ -1,6 +1,20 @@
 #import "com_scalarmachine_skijaawt_SkiaMetalCanvas.h"
 #import "jawt/jawt_helper.h"
 
+#import <Metal/Metal.h>
+#import <Cocoa/Cocoa.h>
+#import <QuartzCore/QuartzCore.h>
+#import <simd/simd.h>
+
+bool inited = false;
+id <MTLDevice> device;
+id <MTLCommandQueue> queue;
+CAMetalLayer *layer;
+
+id <MTLRenderPipelineState> rps;
+
+bool initialize();
+
 /*
  * Class:     com_scalarmachine_skijaawt_SkiaMetalCanvas
  * Method:    nPing
@@ -17,7 +31,21 @@ JNIEXPORT jint JNICALL Java_com_scalarmachine_skijaawt_SkiaMetalCanvas_nPing
  * Signature: ()I
  */
 JNIEXPORT jint JNICALL Java_com_scalarmachine_skijaawt_SkiaMetalCanvas_nInitialize
-  (JNIEnv *, jobject) {
+  (JNIEnv *env, jobject canvas) {
+    JawtHelper helper(env, canvas);
+
+    [CATransaction begin];
+
+    if (!initialize()) {
+      [CATransaction commit];
+      return -1;
+    }
+
+    id <JAWT_SurfaceLayers> surfaceLayers = (id) helper.dsi->platformInfo;
+    surfaceLayers.layer = layer;
+
+    [CATransaction commit];
+
     return 0;
   }
 
@@ -30,3 +58,79 @@ JNIEXPORT jint JNICALL Java_com_scalarmachine_skijaawt_SkiaMetalCanvas_nRender
   (JNIEnv *, jobject) {
     return 0;
   }
+
+bool initialize() {
+  if (inited) return true;
+
+  device = MTLCreateSystemDefaultDevice();
+  queue = [device newCommandQueue];
+  layer = [CAMetalLayer layer];
+  layer.device = device;
+  layer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+  CGColorRef color = CGColorCreateGenericRGB(0.5f, 1.0f, 0.5f, 1.0f);
+  layer.backgroundColor = color;
+
+
+  // layer.displaySyncEnabled = YES;
+
+  // layer.layoutManager = [CAConstraintLayoutManager layoutManager];
+  // layer.autoresizingMask = kCALayerHeightSizable | kCALayerWidthSizable;
+  // layer.contentsGravity = kCAGravityTopLeft;
+  // layer.magnificationFilter = kCAFilterNearest;
+  // NSColorSpace *cs = surfaceLayers.windowLayer.colorspace;
+  // layer.colorspace = cs.CGColorSpace;
+
+  // CGRect frame = CGRectMake(0, 0, backingSize.width, backingSize.height);
+  // NSView *view = [[NSView alloc] initWithFrame:frame];
+  // view.layer = layer;
+  // view.wantsLayer = YES;
+
+
+  // MTLCompileOptions *compileOptions = [MTLCompileOptions new];
+  // compileOptions.languageVersion = MTLLanguageVersion1_1;
+  // NSError *compileError;
+  // id <MTLLibrary> lib = [device newLibraryWithSource:
+  //                 @"#include <metal_stdlib>\n"
+  //                 "using namespace metal;\n"
+  //                 "vertex float4 v_simple(\n"
+  //                 "    constant float4* in  [[buffer(0)]],\n"
+  //                 "    uint             vid [[vertex_id]])\n"
+  //                 "{\n"
+  //                 "    return in[vid];\n"
+  //                 "}\n"
+  //                 "fragment float4 f_simple(\n"
+  //                 "    float4 in [[stage_in]])\n"
+  //                 "{\n"
+  //                 "    return float4(1, 0, 0, 1);\n"
+  //                 "}\n"
+  //                                             options:compileOptions error:&compileError];
+  // if (!lib) {
+  //   NSLog(@"can't create library: %@", compileError);
+  //   return false;
+  // }
+
+  // id <MTLFunction> vs = [lib newFunctionWithName:@"v_simple"];
+  // assert(vs);
+  // id <MTLFunction> fs = [lib newFunctionWithName:@"f_simple"];
+  // assert(fs);
+
+  // id <MTLCommandQueue> cq = [device newCommandQueue];
+  // assert(cq);
+
+  // MTLRenderPipelineDescriptor *rpd = [MTLRenderPipelineDescriptor new];
+  // rpd.vertexFunction = vs;
+  // rpd.fragmentFunction = fs;
+  // rpd.colorAttachments[0].pixelFormat = layer.pixelFormat;
+  // rps = [device newRenderPipelineStateWithDescriptor:rpd error:nullptr];
+  // assert(rps);
+
+  // CFRelease(cq);
+  // CFRelease(lib);
+
+  inited = true;
+
+  printf("initialized\n");
+
+  return true;
+}
