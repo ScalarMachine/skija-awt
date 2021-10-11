a7c6bc1f25bce4760d05ce0db4b69a98a8195d93
bumfo <bumfo@users.noreply.github.com>
2021-10-11 04:47:30 +0000

    add test java

---
+++ src/test/java/com/scalarmachine/skijaawt/macos/SkiaMetalCanvasTest.java
@@ -0,0 +1,38 @@
+package com.scalarmachine.skijaawt;
+
+// build: javac com/scalarmachine/**/macos/*.java
+// run: java -Djava.library.path=../../../ com.scalarmachine.skijaawt.SkiaMetalCanvasTest
+
+public class SkiaMetalCanvasTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Frame frame = new Frame();
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override public void windowClosing(WindowEvent ev) {
+                System.exit(0);
+            }
+        });
+
+        SkiaMetalCanvas canvas = new SkiaMetalCanvas();
+        frame.add(canvas);
+
+        EventQueue.invokeLater(() -> {
+            canvas.nInitialize();
+            frame.setVisible(true);
+            frame.transferFocus();
+
+            canvas.oldTime = System.nanoTime();
+
+            new Thread(() -> {
+                while (true) {
+                    if (!canvas.isValid()) return;
+                    canvas.nRender();
+                }
+            }).start();
+        });
+    }
+}
