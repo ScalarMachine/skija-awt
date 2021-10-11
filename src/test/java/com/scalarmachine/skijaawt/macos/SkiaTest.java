1c87f2df47fcee073fa09d6ded51fdf2ef51c2e1
bumfo <bumfo@users.noreply.github.com>
2021-10-11 06:48:14 +0000

    commit

---
+++ src/test/java/com/scalarmachine/skijaawt/macos/SkiaTest.java
@@ -0,0 +1,50 @@
+package com.scalarmachine.skijaawt;
+
+import org.jetbrains.skija.*;
+import org.jetbrains.skija.impl.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class SkiaTest {
+    public static void main(String[] args) {
+        System.out.println(SkiaMetalCanvas.nPing());
+
+        Frame frame = new Frame();
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override public void windowClosing(WindowEvent ev) {
+                System.exit(0);
+            }
+        });
+
+        SkiaMetalCanvas canvas = new SkiaMetalCanvas();
+        frame.add(canvas);
+
+        EventQueue.invokeLater(() -> {
+            canvas.nInitialize();
+            frame.setVisible(true);
+            frame.transferFocus();
+
+            // canvas.oldTime = System.nanoTime();
+
+            new Thread(() -> {
+                while (true) {
+                    if (!canvas.isValid()) return;
+                    canvas.nBeginRender();
+
+                    BackendRenderTarget renderTarget = BackendRenderTarget.makeMetal(
+                        (int) 200,
+                        (int) 200,
+                        0
+                    );
+
+                    // canvas.nRender();
+                    canvas.nSwapBuffers();
+                }
+            }).start();
+        });
+    }
+}
